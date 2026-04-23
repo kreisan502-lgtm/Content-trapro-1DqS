b8c17fef0997f3636ffcd40637d8ab18c1459a00
kreisan502-lgtm <kreisan502@gmail.com>
2026-04-24 06:51:30 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# config.py
+
+# Ganti URL di bawah ini dengan URL Web App terbaru dari Google Apps Script kamu
+SCRIPT_URL = "https://script.google.com/macros/s/AKfycbx...MASUKKAN_URL_KAMU_DI_SINI.../exec"
+
+# Kamu juga bisa menambah konfigurasi lain di sini jika diperlukan
+APP_NAME = "BizInvest VIP Suite"
+VERSION = "2.0"
