06a4ad35a841effe66e8e3115d22459b85b7e18b
kreisan502-lgtm <kreisan502@gmail.com>
2026-04-23 12:27:35 +0700

    Create panduan.py

---
+++ panduan.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+def show_content():
+    st.write("Selamat datang di panduan pemula.")
+    with st.expander("Langkah 1: Mengenal Instrumen"):
+        st.write("Isi panduan sangat rinci di sini...")
+      
