a2864d3973ebd62c87d5f61792aabdc133678706
kreisan502-lgtm <kreisan502@gmail.com>
2026-04-23 12:28:01 +0700

    Create book_guide.py

---
+++ book_guide.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+def show_content():
+    st.write("Daftar bacaan penting.")
+    with st.expander("Strategi Investasi Value Investing"):
+        st.write("Isi buku strategi di sini...")
+      
