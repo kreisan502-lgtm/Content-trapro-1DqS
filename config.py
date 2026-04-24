29841aa5df061dc31f8ede4ef6c7eb77fc2a5676
kreisan502-lgtm <kreisan502@gmail.com>
2026-04-24 07:24:30 +0700

    Update config.py

--- config.py
+++ config.py
@@ -4,4 +4,7 @@
 CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTyzO1OA-Cq8Tabx9KODOX9VWNFTZ0Gluuja8qztT30GR7c2FbPoJLBs_F1h8KRtRYnW6SRbKM1jpu1/pub?gid=0&single=true&output=csv"
 
 # URL Deployment dari Google Apps Script
-SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzWtwBOht7__Na97Bf7HCQq3B8BFmI9stxS7zAoKL957wLsd1oBOCgY6IGdbxyXHNG3/exec"
+SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzAX6LmbmDxNc6nIUnqgDAyYFdjA4tWSkxn0_TugdTv7bAtA2_QfFq8ByheO3EkG67o/exec"
+
+# Link Order Lynk.id
+LINK_ORDER = "https://lynk.id/nore30"
